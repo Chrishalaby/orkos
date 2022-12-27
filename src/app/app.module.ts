1d8fc34a042ace3cbb793868dfce6e1dc96fb6a6
Chris Halabi <chridtianhalabi@gmail.com>
2022-12-27 22:44:50 +0200

    first push base down

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {CarouselModule} from 'primeng/carousel';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { HomeComponent } from './home/home.component';
+import {HttpClientModule} from '@angular/common/http';
+import {ButtonModule} from 'primeng/button';
+import {DialogModule} from 'primeng/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {SidebarModule} from 'primeng/sidebar';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent,
+    TopBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CarouselModule,
+    HttpClientModule,
+    ButtonModule,
+    DialogModule,
+    BrowserAnimationsModule,
+    SidebarModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+export interface Product {
+  image: string,
+  name: string,
+  price: number,
+  inventoryStatus: string,
+  description: string,
+}
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+
+export class HomeComponent implements OnInit {
+
+  products: Product[] = [];
+  responsiveOptions;
+  displayModal: boolean | undefined;
+
+  constructor(private readonly httpClient: HttpClient, private router: Router) {
+    this.responsiveOptions = [
+      {
+          breakpoint: '1024px',
+          numVisible: 3,
+          numScroll: 3,
+          showIndicators: false
+      },
+      {
+          breakpoint: '768px',
+          numVisible: 2,
+          numScroll: 2,
+          showIndicators: false
+      },
+      {
+          breakpoint: '560px',
+          numVisible: 1,
+          numScroll: 1,
+          showIndicators: false
+      }
+  ];
+  }
+
+  ngOnInit() {
+      this.httpClient.get<any>('assets/products.json').pipe(tap((products: any)=> {this.products = products.data;})).subscribe();
+  }
+
+  showModalDialog() {
+    this.displayModal = true;
+  }
+}
